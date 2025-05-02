68cec90ccfb23c7004c4ab4cd5869e29ade792d9
Dipak Prajapati <dipakp@logicrays.com>
2025-05-02 16:55:51 +0530

    Contact form Create Controller, Model, DB migration

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ContactController;
 
 /*
 |--------------------------------------------------------------------------
@@ -30,5 +31,7 @@ Route::middleware('auth')->group(function () {
 });
 
 Route::get('/products', [ProductController::class, 'index']);
+Route::resource('contacts', ContactController::class);
+
 
 require __DIR__.'/auth.php';
