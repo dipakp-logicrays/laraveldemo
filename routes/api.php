c46dad9a3b20f77a80e3b3fd7f60de6b44f34573
Dipak Prajapati <dipakp@logicrays.com>
2025-05-07 12:48:16 +0530

    Faq Register API routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\FAQController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,21 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+/*
+|--------------------------------------------------------------------------
+| API Routes for FAQ
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for the FAQ module.
+|
+*/
+Route::prefix('faqs')->group(function () {
+    Route::get('/', [FAQController::class, 'index']);
+    Route::post('/', [FAQController::class, 'store']);
+    Route::get('/{id}', [FAQController::class, 'show']);
+    Route::put('/{id}', [FAQController::class, 'update']);
+    Route::delete('/{id}', [FAQController::class, 'destroy']);
 });
