89f5adb6491ba33ee97291f07130f26e1060c1af
Dipak Prajapati <dipakp@logicrays.com>
2025-05-09 11:32:04 +0530

    Only Logged in users can access these routes: contacts, faqs, products

--- routes/web.php
+++ routes/web.php
@@ -29,10 +29,13 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Only Logged in users can access these routes
+    Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+    Route::resource('contacts', ContactController::class);
+    Route::get('/faqs', [FaqController::class, 'index'])->name('faqs.index');
 });
 
-Route::get('/products', [ProductController::class, 'index'])->name('products.index');
-Route::resource('contacts', ContactController::class);
-Route::get('/faqs', [FaqController::class, 'index'])->name('faqs.index');
+
 
 require __DIR__.'/auth.php';
